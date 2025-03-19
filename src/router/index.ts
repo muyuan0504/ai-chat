9e4c7de58dafe0950ec037d51cdd7a95a07713fd
aiden <972523881@qq.com>
2025-03-19 20:09:54 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const AiChat = () => import('@/views/ai-chat/index')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/ai-chat',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/ai-chat',
+      name: 'aiChat',
+      component: AiChat,
     },
   ],
 })
